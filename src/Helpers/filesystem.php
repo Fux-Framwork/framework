2c2531d37dac7dcf213d87a461ddad59d242539c
Matteo Fusillo <58771053+mfusillo98@users.noreply.github.com>
2023-10-03 15:07:58 +0200

    Fix helpers files import

--- src/Helpers/filesystem.php
+++ src/Helpers/filesystem.php
@@ -41,16 +41,17 @@ function download($filepath, $filename, $contentType = 'application/force-downlo
     readfile($filepath);
 }
 
+if (!function_exists('mime_content_type')) {
+    function mime_content_type($filename)
+    {
+        $result = new finfo();
 
-function mime_content_type($filename)
-{
-    $result = new finfo();
+        if (is_resource($result) === true) {
+            return $result->file($filename, FILEINFO_MIME_TYPE);
+        }
 
-    if (is_resource($result) === true) {
-        return $result->file($filename, FILEINFO_MIME_TYPE);
+        return false;
     }
-
-    return false;
 }
 
 function mime2ext($mime)
